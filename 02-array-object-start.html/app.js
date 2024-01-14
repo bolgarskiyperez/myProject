8d25fb8593d93e50f876499ebbe295fe8eaaa180
bolgarskiyperez <152485986+bolgarskiyperez@users.noreply.github.com>
2024-01-14 13:31:37 +0200

    14 01 2024

--- 02-array-object-start.html/app.js
+++ 02-array-object-start.html/app.js
@@ -1,41 +1,70 @@
-const inputElement = document.getElementById('title')
-const createBtn = document.getElementById('create')
-const listElement = document.getElementById('list')
-const notes = ['1', '2']
+const inputElement = document.getElementById('title');
+const createBtn = document.getElementById('create');
+const listElement = document.getElementById('list');
+
+const notes = [
+    {
+        title: 'записать блок',
+        completed: false
+    },
+    {
+        title: '1231',
+        completed: true,
+    }
+];
 
 function render() {
-    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[0]))
-    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[1]))
-
+    listElement.innerHTML = '';
+    if (notes.length === 0) {
+        listElement.innerHTML = '<p>Нет элементов</p>'
+    }
+    for (let i = 0; i < notes.length; i++) {
+        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i));
+    }
 }
+
+render();
 
 createBtn.onclick = function () {
-    // listElement.innerHTML = 
-    if (inputElement.value.lenght === 0) {
-        return 
+    if (inputElement.value.length === 0) {
+        return;
     }
-    
-    listElement.insertAdjacentHTML(
-        'beforeend',
-        getNoteTemplate(inputelement.value)
-    )
 
-    inputElement.value = ''
-    
+    const newNote = {
+        title: inputElement.value,
+        completed: false,
+    };
 
+    notes.push(newNote);
+    render();
 
-}
+    inputElement.value = '';
+};
 
-function getNoteTemplate(title) {
+listElement.onclick = function (event) {
+    if (event.target.dataset.index) {
+        const index = parseInt(event.target.dataset.index);
+        const type = event.target.dataset.type;
+
+        if (type === 'toggle') {
+            notes[index].completed = !notes[index].completed;
+        } else if (type === 'remove') {
+            notes.splice(index, 1)
+        }
+
+        render()
+    }
+};
+
+function getNoteTemplate(note, index) {
     return `
     <li class="list-group-item d-flex justify-content-between align-items-center">
-        <span>${inputElement.value}</span>
+        <span class="${note.completed ? 'text-decoration-line-through' : ''}">${note.title}</span>
         <span>
-            <span class="btn btn-small btn-success">&check;</span>
-            <span class="btn btn-small btn-danger">&times;</span>
+            <span class="btn btn-small btn-${note.completed ? 'warning' : 'success'}"
+            data-index="${index}" data-type="toggle">&check;</span>
+            <span class="btn btn-small btn-danger" data-index="${index}" data-type="remove">&times;</span>
         </span>
     </li>
-
-`
-
+    `;
 }
